d84425cf9d0f6ab8f0e8ce1b96ef93e071421258
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-10-04 16:03:43 +0100

    Finished the first test class: FullLocalhostUriTest

--- tests/QuillStack/Uri/FullLocalhostUriTest.php
+++ tests/QuillStack/Uri/FullLocalhostUriTest.php
@@ -67,4 +67,60 @@ final class FullLocalhostUriTest extends TestCase
         $this->assertEquals(MockFullLocalhost::SCHEME, $this->uri->getScheme());
         $this->assertEquals('https', $uri->getScheme());
     }
+
+    public function testWithUserInfo()
+    {
+        $uri = $this->uri->withUserInfo('test-user');
+
+        $this->assertEquals(MockFullLocalhost::USER_INFO, $this->uri->getUserInfo());
+        $this->assertEquals('test-user', $uri->getUserInfo());
+    }
+
+    public function testWithUserInfoWithPassword()
+    {
+        $uri = $this->uri->withUserInfo('test-user:new-password');
+
+        $this->assertEquals(MockFullLocalhost::USER_INFO, $this->uri->getUserInfo());
+        $this->assertEquals('test-user:new-password', $uri->getUserInfo());
+    }
+
+    public function testWithHost()
+    {
+        $uri = $this->uri->withHost('localhost');
+
+        $this->assertEquals(MockFullLocalhost::HOST, $this->uri->getHost());
+        $this->assertEquals('localhost', $uri->getHost());
+    }
+
+    public function testWithPort()
+    {
+        $uri = $this->uri->withPort(8081);
+
+        $this->assertEquals(MockFullLocalhost::PORT, $this->uri->getPort());
+        $this->assertEquals(8081, $uri->getPort());
+    }
+
+    public function testWithPath()
+    {
+        $uri = $this->uri->withPath('new-path');
+
+        $this->assertEquals(MockFullLocalhost::PATH, $this->uri->getPath());
+        $this->assertEquals('new-path', $uri->getPath());
+    }
+
+    public function testWithQuery()
+    {
+        $uri = $this->uri->withQuery('x=3&y=6');
+
+        $this->assertEquals(MockFullLocalhost::QUERY, $this->uri->getQuery());
+        $this->assertEquals('x=3&y=6', $uri->getQuery());
+    }
+
+    public function testWithFragment()
+    {
+        $uri = $this->uri->withFragment('strawberry');
+
+        $this->assertEquals(MockFullLocalhost::FRAGMENT, $this->uri->getFragment());
+        $this->assertEquals('strawberry', $uri->getFragment());
+    }
 }
